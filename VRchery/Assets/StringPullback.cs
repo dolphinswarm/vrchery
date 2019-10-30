a91e40b75ac1b8d6e0ec44d0e4534ef9bd5ce326
schmitb3 <48733409+schmitb3@users.noreply.github.com>
2019-10-30 19:31:24 -0400

    Added bow object and starting to implement bow pullback

---
+++ VRchery/Assets/StringPullback.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class StringPullback : MonoBehaviour
+{
+    // Bow Pullback
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public OVRInput.Controller left;
+    public OVRInput.Controller right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skinnedMeshRenderer = this.GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate distance between hands for bow pullback animation
+        float distance = Mathf.Max(((OVRInput.GetLocalControllerPosition(left) - OVRInput.GetLocalControllerPosition(right)).magnitude - 0.3f), 0.0f) * 325.0f;
+        Debug.Log(distance);
+
+        // Calculate angle between hands
+        Quaternion leftHandAngle = OVRInput.GetLocalControllerRotation(left);
+        Quaternion rightHandAngle = OVRInput.GetLocalControllerRotation(right);
+
+        // If right bumper pressed, apply pullback animation
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, right))
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight(0, distance);
+        }
+        
+        // Else, set back to 0
+        else
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight(0, 0);
+        }
+        
+    }
+}
